46f04c2f50fee03d5a6f8b4e08a1d0e8d2994481
malekmkaddem <79175484+malekmkaddem@users.noreply.github.com>
2021-05-14 16:28:54 -0700

    Add files via upload

---
+++ BackEnd.js
@@ -0,0 +1,37 @@
+    
+    
+   <!--     <!--  Top menu bar -->
+    <div>
+       <label for = "mycheckbox" class = "menu-toggler"> 
+  <i class="ui blue sticky fixed bars icon" id = "icon"></i>
+            </label>
+            <input type = "checkbox" id = "mycheckbox">
+    <div class="ui top sticky fixed menu main" id = "menu">
+        
+    
+      <nav>  <div id = "menu2">
+  <div class="ui header item" >
+    <a class="BabyProdigy" href="index.html">BabyProdigy</a>
+  </div>
+        <div class=" search item" id = "search">
+      
+        <input type="text" placeholder="Type to search...">
+    
+</div>
+  <a class="item" id = "item"
+     href = "Products.html">
+    Products
+  </a>
+  <a class="item" id = "item" href="Location.html">
+    Location
+  </a>
+        <a class="item" id = "item" href="Extra.html">
+    Extra
+  </a>
+        <a class="item" id = "item" href="ContactUs.html">
+    Contact us
+  </a>
+</div>
+</nav>
+</div>
+    </div>   
